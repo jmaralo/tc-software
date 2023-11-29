62241f320fc3189031bf1d388333c32fa6313f32
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-29 15:48:59 +0100

    Explain a bit the for loop code

--- lesson1/basic-for.js
+++ lesson1/basic-for.js
@@ -1,6 +1,11 @@
+// This program counts from n down to 1
 
 const countdown = 5
 
+// for repeats an action a certain amount of times, it has three parts:
+// first a loop variable (in this case i)
+// then a loop condition (in this case i < countdown)
+// and then how to update the loop variable (in this case add 1 to i)
 for (let i = 0; i < countdown; i++) {
     console.log(countdown - i)
     // sleep for 1 second
