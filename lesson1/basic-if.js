b8e8ac6408c4dc49079b80d6442664ef507d8ecf
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-29 15:42:22 +0100

    Add if example

---
+++ lesson1/basic-if.js
@@ -0,0 +1,14 @@
+// This program takes an age and says if we have the legal age
+
+const age = 20
+
+// The if statement checks a certain condition is met and executes its code if it does
+if (age > 18) {
+    console.log("You are above the legal age")
+// The else if statement will check its condition if the first didn't meet
+} else if (age == 18) {
+    console.log("You have the legal age")
+// The else statement executes if none of the conditions meet
+} else {
+    console.log("You are under the legal age")
+}
