964308cd3c7539e4c66d9cacc448debd1e999182
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-29 15:35:13 +0100

    Variable example

---
+++ lesson1/greet.js
@@ -0,0 +1,10 @@
+// This code writes a greeting based on the name and surname
+
+const my_name = "John"
+const my_surname = "Doe"
+
+// we can concatenate strings by using the '+' operator
+console.log("Hello, " + my_name + " " + my_surname)
+
+// Or by using this syntax we can embed any variable in a string
+console.log(`Hello, ${my_name} ${my_surname}`)
