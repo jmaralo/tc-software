87f5608d684c63ae8035cb6d120fff72bf718450
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-29 15:42:33 +0100

    Explain boolean operators a bit

--- lesson1/basic-if.js
+++ lesson1/basic-if.js
@@ -3,6 +3,8 @@
 const age = 20
 
 // The if statement checks a certain condition is met and executes its code if it does
+// ifs take a 'boolean' value, which can only be "true" or "false"
+// we can compare values using '>', '<', '>=', '<=', '==' and '!='
 if (age > 18) {
     console.log("You are above the legal age")
 // The else if statement will check its condition if the first didn't meet
@@ -11,4 +13,12 @@ if (age > 18) {
 // The else statement executes if none of the conditions meet
 } else {
     console.log("You are under the legal age")
+}
+
+// We can also compare booleans with boolean operators, these are
+// And '&&' (both conditions are true)
+// Or '||' (at least one condition is true)
+// Not '!' (the condition isn't true)
+if (!(age > 18) && !(age < 18)) {
+    console.log("You have the legal age")
 }
